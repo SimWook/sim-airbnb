4199424e390bfe000e4b81113bc709a7fb4842ae
Sim Wook <wook.sim@gmo.media>
2025-04-24 12:50:40 +0900

    create categories完成

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rooms.apps.RoomsConfig",
     "common.apps.CommonConfig",
     "experiences.apps.ExperiencesConfig",
+    "categories.apps.CategoriesConfig",
 ]
 
 MIDDLEWARE = [
